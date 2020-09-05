881fca23b4fba0c030be6d9f08c2481ed6297d63
Srdjan294 <srdjanfilipovic991@gmail.com>
2020-09-05 13:27:13 +0200

    Prosljeđivanje parametara iz konrtolera u view

--- controller/IndexController.php
+++ controller/IndexController.php
@@ -13,4 +13,41 @@ class IndexController extends Controller
     {
         $this->view->render('onama');
     }
+
+    public function login(){
+        $this->view->render('login',[
+            'email'=> '',
+            'poruka'=> 'Popunite tražene podatke'
+        ]);
+    }
+
+    public function autorizacija(){
+
+       if(!isset($_POST['email']) || !isset($_POST['lozinka'])){
+            $this->login();
+            return; // short curcuiting
+       }
+
+        if(strlen(trim($_POST['email']))===0){
+            $this->view->render('login',[
+                'email'=> '',
+                'poruka'=> 'Obavezan unos email-a'
+            ]);
+            return;
+        }
+
+
+        if(strlen(trim($_POST['lozinka']))===0){
+            $this->view->render('login',[
+                'email'=> trim($_POST['email']),
+                'poruka'=> 'Obavezan unos lozinke'
+            ]);
+            return;
+        }
+
+        // 100% siguran da imaš email i lozinku
+
+
+
+    }
 }
