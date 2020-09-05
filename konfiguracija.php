d8c269bfc005ebd4db19779b678c9685715d14af
Srdjan294 <srdjanfilipovic991@gmail.com>
2020-09-05 13:32:56 +0200

    Priprema konfiguracije s podacima za bazu

--- konfiguracija.php
+++ konfiguracija.php
@@ -1,8 +1,27 @@
 <?php
 
+$dev=$_SERVER['REMOTE_ADDR'] === '127.0.0.1' ? true : false;
+
+if($dev){
+    $baza=[
+        'server'=>'localhost',
+        'baza'=>'edunovapp21',
+        'korisnik'=>'edunova',
+        'lozinka'=>'edunova'
+    ];
+}else{
+    $baza=[
+        'server'=>'localhost',
+        'baza'=>'hermes_pp21',
+        'korisnik'=>'hermes_edunova',
+        'lozinka'=>'hermes_edunova'
+    ];
+}
+
 
 return [
-    'dev' => $_SERVER['REMOTE_ADDR'] === '127.0.0.1' ? true : false,
+    'dev' => $dev,
     'nazivAPP' => 'Edunova APP',
-    'url' => 'http://polaznik14.edunova.hr/'
+    'url' => 'http://polaznik14.edunova.hr/',
+    'baza' =>$baza
 ];
