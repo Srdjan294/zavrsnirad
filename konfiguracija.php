355a2d2ab6aacf925f1e321dda56730d36a379a2
Srdjan294 <srdjanfilipovic991@gmail.com>
2020-09-05 13:19:37 +0200

    Slanje podataka iz kontrolera na view

--- konfiguracija.php
+++ konfiguracija.php
@@ -2,7 +2,7 @@
 
 
 return [
-    dev' => $_SERVER['REMOTE_ADDR'] === '127.0.0.1' ? true : false,
+    'dev' => $_SERVER['REMOTE_ADDR'] === '127.0.0.1' ? true : false,
     'nazivAPP' => 'Edunova APP',
     'url' => 'http://polaznik14.edunova.hr/'
 ];
